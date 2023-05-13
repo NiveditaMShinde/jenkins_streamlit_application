17bf08274f5af489ace68290d0d357ed0c61908d
NiveditaMShinde <66831307+NiveditaMShinde@users.noreply.github.com>
2023-05-13 16:52:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ cd /home/ec2-user/
 sudo netstat -tnlp | grep 8501
 if [ $(echo $?) = 1 ]
 then
-  	screen -m -d stramlit run app.py
+  	screen -m -d streamlit run app.py
 else
       fuser -k 8501/tcp
       screen -m -d streamlit run app.py
